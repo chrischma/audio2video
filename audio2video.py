f6f9d9fe6e90c5982e960681a8b64190037b6819
Chris <post@christoph-schmalfuss.de>
2020-10-30 16:05:36 +0100

    Add files via upload

---
+++ audio2video.py
@@ -0,0 +1,14 @@
+import os
+
+audio = input("Drop your Audio File here: ")
+image = input("Drop your image here:")
+
+input_name = audio.split(".")
+output_name = input_name[0]+".mp4"
+#filename = input ("Enter a Name for your file here: ")+"_converted.mp4"
+command = f'''ffmpeg -y -loop 1 -framerate 2 -i {image} -i {audio}  -c:v libx264 -tune stillimage -c:a aac -b:a 320k -shortest -fflags +shortest -max_interleave_delta 100M {output_name}
+'''
+
+os.system(command)
+
+print(f' \n Your file is ready. You find it here: \n {output_name} \n \n Have a nice day!')
